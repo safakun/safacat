9928a27aa7f90a82d6742f265a61548047f64b2f
safakun <safarovdeem@gmail.com>
2023-09-13 18:04:44 +0500

    added client_server function

--- safacat.py
+++ safacat.py
@@ -33,6 +33,41 @@ def usage():
     sys.exit(0)
 
 
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # connect to our target host
+        client.connect((target,port))
+        if len(buffer):
+            client.send(buffer)
+        while True:
+            # now wait for data back
+            recv_len = 1
+            response = ""
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data) 
+                response += data
+     
+                if recv_len < 4096:
+                    break
+
+            print(response)
+        # wait for more input
+        buffer = raw_input("")
+        buffer += "\n"
+        # send it off
+        client.send(buffer)
+    
+    except:
+        print("Exceptio! Exiting")
+        # tear down the connection
+        client.close()
+        
+
+            
+
+
 def main():
     global listen
     global port
