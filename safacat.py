48f028675e40ed1e6f8b9852d3ded96c605c74eb
safakun <safarovdeem@gmail.com>
2023-09-13 18:13:53 +0500

    added server_loop function

--- safacat.py
+++ safacat.py
@@ -65,7 +65,21 @@ def client_sender(buffer):
         client.close()
         
 
-            
+
+def server_loop():
+    global target 
+    # if no target is defined, we listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target,port))
+    server.listen(5)
+    
+    while True:
+        client_socket, addr = server.accept()
+        # spin off a thread to handle our new client
+    client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+    client_thread.start()          
 
 
 def main():
